67d7fcb8e341ee749d76c4e11b5fe00ee198f1e3
Tiago Cunha <tcunha_lp@hotmail.com>
2019-04-15 17:45:14 +0100

    cors update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,11 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
 
-    resource 'riders',
-      headers: :any,
-      methods: :get
-
-    resource 'submissions',
+    resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
